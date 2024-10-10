135093200985c2175d9d5e4189d416a27fd019e2
Pablo Enguix Llopis <56578000+PabloEnguix09@users.noreply.github.com>
2024-10-10 12:01:23 +0000

    Project done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const getRandomNumber = (maxNumber) => {
+    return Math.floor(Math.random()*maxNumber);
+  };
+
+  let excuseElement = document.getElementById("excuse");
+  let excuse = who[getRandomNumber(who.length)] + " " + action[getRandomNumber(action.length)] + " " + what[getRandomNumber(what.length)] + " " + when[getRandomNumber(when.length)];
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
